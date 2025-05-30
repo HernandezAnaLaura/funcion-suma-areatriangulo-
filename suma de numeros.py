d134959aaa4d86bc87b699733369709190257700
Hernandez ana laura <ana.lupsi09@gmail.com>
2025-05-30 09:42:19 -0600

    Add files via upload

---
+++ suma de numeros.py
@@ -0,0 +1,8 @@
+def suma_numeros(num1, num2):
+    return num1 + num2
+    return num1+num2
+   
+num1 = float(input("ingresa num1:"))
+num2 = float(input("ingresa num2:"))
+resultado = suma_numeros(num1, num2)
+print ("la suma es:", resultado)
